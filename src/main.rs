94420fcae32708750aed9c00f1b167743e2583aa
sklbz <122096315+sklbz@users.noreply.github.com>
2024-03-30 13:35:39 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ use rand::Rng;
 use rand::thread_rng;
 
 fn main() {
-		println!("Guess the number!");
+	let mut rng = thread_rng();
+	let random_number_in_range: u32 = rng.gen_range(-1..=37);
+	
+	println!("Guess the number!\n(Hint: it's between -1 and 37)");
 
-    println!("Please input your guess.");
+	println!("Please input your guess.");
 
-    let mut guess = String::new();
+	let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+	io::stdin()
+		.read_line(&mut guess)
+		.expect("Failed to read line");
 
     println!("You guessed: {guess}");
 }
