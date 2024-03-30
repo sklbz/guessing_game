087b7ced18104ed190bf0174f377ea0178e56008
sklbz <122096315+sklbz@users.noreply.github.com>
2024-03-30 13:37:39 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::thread_rng;
 
 fn main() {
 	let mut rng = thread_rng();
-	let random_number_in_range: u32 = rng.gen_range(-1..=37);
+	let random_number: i32 = rng.gen_range(-1..=37);
 	
 	println!("Guess the number!\n(Hint: it's between -1 and 37)");
 
@@ -16,5 +16,6 @@ fn main() {
 		.read_line(&mut guess)
 		.expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+	println!("You guessed: {guess}");
+	println!("The right answer was: {random_number}!");
 }
